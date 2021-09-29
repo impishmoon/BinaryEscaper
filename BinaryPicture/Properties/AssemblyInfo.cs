9670d8247f56a6fe6d8f2660c677efe3f4a66aaf
ZeroByter <romgamesrom@gmail.com>
2021-09-29 23:33:28 +0300

    Changed assembly info slightly...

--- BinaryPicture/Properties/AssemblyInfo.cs
+++ BinaryPicture/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BinaryEscaper")]
+[assembly: AssemblyTitle("Sending bytes where they weren't meant to go...")]
 [assembly: AssemblyDescription("Sending bytes where they weren't meant to go...")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
